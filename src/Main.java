5fe0a24292d1b07bb6957313c1fc83f0d7967911
Tural <musaev.tural.94@mail.ru>
2024-04-25 00:53:05 +0300

    домашка строки 11

--- src/Main.java
+++ src/Main.java
@@ -1,23 +1,23 @@
 import java.sql.SQLOutput;
 public class Main{
-   public static void main(String[]args){
+   public static void main(String[]args) {
            System.out.println("задача 1");
-           String firstName ="Ivan";
-           String middleName ="Ivanovich";
-           String lastName ="Ivanov";
+           String firstName = "Ivan";
+           String middleName = "Ivanovich";
+           String lastName = "Ivanov";
            String fullName = lastName + " " + firstName + " " + middleName;
-           System.out.println("Ф.И.О. сотрудни -" + fullName);
+           System.out.println("Ф.И.О. сотрудника - " + fullName);
            System.out.println();
 
            System.out.println("задача 2");
            String fullNameNew = "Ivanov Ivan Ivanovih";
            String capsLetter = fullNameNew.toUpperCase();
-           System.out.println("Данные Ф.И.О.сотрудника для заполнения отчета -" + capsLetter);
+           System.out.println("Данные Ф.И.О. сотрудника для заполнения отчета - " + capsLetter);
            System.out.println();
 
            System.out.println("задача 3");
-           String fullNameNewTwo = "Иванов Семён Семёнович";
-           String replacedFullName = fullNameNewTwo.replace("ё","е");
+           String fullAppellation = "Иванов Семён Семёнович";
+           String replacedFullName = fullAppellation.replace("ё","е");
            System.out.println("Данные Ф.И.О. сотрудника - " + replacedFullName);
 
    }
